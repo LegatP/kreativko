bcd97fb64c27d4e6de04160d07a4987ed0408d49
Primož Legat <>
2025-09-26 22:46:41 +0200

    Add vercel analytics

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,10 +3,12 @@
 import { HeroUIProvider, ToastProvider } from "@heroui/react";
 import { AppContextProvider } from "../components/contexts/AppContext";
 import FirebaseProvider from "@/components/firebase/FirebaseProvider";
+import { Analytics } from "@vercel/analytics/next";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <HeroUIProvider>
+      <Analytics />
       <ToastProvider placement="top-right" />
       <FirebaseProvider>
         <AppContextProvider>{children}</AppContextProvider>
