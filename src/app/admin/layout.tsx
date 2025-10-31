f4e65ef6303779f66cca3d37d13eafd8ba1d734a
Primož Legat <legatprimoz@gmail.com>
2025-10-31 21:12:35 +0100

    Remove middleware

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,8 +1,19 @@
 "use client";
 
 import AdminNavigation from "@/components/layout/AdminNavigation";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const { replace } = useRouter();
+
+  useEffect(() => {
+    console.log("Admin layout loaded");
+    if (process.env.NODE_ENV === "production") {
+      replace("/");
+    }
+  }, [replace]);
+
   return (
     <div className="pl-20 py-10 pr-10">
       <AdminNavigation />
